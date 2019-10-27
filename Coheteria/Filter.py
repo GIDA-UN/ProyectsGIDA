9d51a21d309f38b3bfd9949edefdb291e250cedd
damendozam <diego.mendoza@scrummers.co>
2019-10-26 21:47:42 -0500

    Adición de análisis de resultados de las pruebas de empuje

--- Coheteria/Filter.py
+++ Coheteria/Filter.py
@@ -18,7 +18,11 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
 # Filter requirements.
 order = 6
 fs = 1000      # sample rate, Hz
-cutoff = 25  # desired cutoff frequency of the filter, Hz
+
+cutoff = 15  # desired cutoff frequency of the filter, Hz
+index=10
+textFile='P'+str(index)+'/P'+str(index)+'.txt'
+textSave='P'+str(index)+'/result.txt'
 
 # Get the filter coefficients so we can check its frequency response.
 b, a = butter_lowpass(cutoff, fs, order)
@@ -30,21 +34,23 @@ plt.plot(0.5*fs*w/np.pi, np.abs(h), 'b')
 plt.plot(cutoff, 0.5*np.sqrt(2), 'ko')
 plt.axvline(cutoff, color='k')
 plt.xlim(0, 0.5*fs)
-plt.title("Lowpass Filter Frequency Response")
+plt.title("Lowpass Filter Frequency Response (cutoff="+str(cutoff)+")")
 plt.xlabel('Frequency [Hz]')
 plt.grid()
 
 
 # Demonstrate the use of the filter.
 # First make some data to be filtered.
-t = np.linspace(0, 3.228, 3228, endpoint=False)
+
 # "Noisy" data.  We want to recover the 1.2 Hz signal from this.
 #>data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
 
-with open('test.txt') as f:
+#with open('test.txt') as f:
+with open('Empuje/'+textFile) as f:
     dataIn = f.read().splitlines() 
 data=[]
-count=1
+sizeofData=len(dataIn)
+t = np.linspace(0, sizeofData/1000, sizeofData, endpoint=False)
 for i in dataIn:
     data.append(float(i))
 
@@ -70,7 +76,7 @@ plt.grid()
 plt.legend()
 plt.show()
 
-f=open('result.txt','a')
+f=open('Empuje/'+textSave,'a')
 
 for x in y:
     #print(str(x)+'\n')
